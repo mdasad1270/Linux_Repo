4b91c5dad0d57acacd576a115339f30d85ff72da
mdasad1270 <148677457+mdasad1270@users.noreply.github.com>
2026-04-18 16:24:53 +0530

    Update service_check.sh

--- project/service_check.sh
+++ project/service_check.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-read -p "Enter service name :- " name
+while true
+do
+    read -p "Enter Service Name :- " name
 
-if which $name &> /dev/null;then
-    echo "$name already exist..!"
-    read -p "Want to install other software? if yes press 1 or press 0 to exist :- " a
-    if [ $a -eq 1 ];then
-        read -p "enter service name :- " service
-        sudo apt install $service -y
-        echo "Installation Completed..!"
+    if command -v "$name" &> /dev/null
+    then
+        echo "$name already exists."
+
+        read -p "Do you want to try another service? (y/n): " choice
+
+        if [ "$choice" == "n" ]; then
+            echo "Exiting the script..."
+            break
+        fi
+
     else
-        echo "Exited the Script."
+        echo "$name doesn't exist."
+
+        read -p "Do you want to install it? (y/n): " choice2
+
+        if [ "$choice2" == "y" ]; then
+            sudo apt install "$name" -y
+            echo "Installation Completed!"
+        fi
+
+        #Exit after handling non-existing service
+        echo "Exiting the script..."
+        break
     fi
-else
-    read -p "$name doesn't exist? if you want to install press 1 or press 0 to exist :- " b
-    if [ $b -eq 1 ];then
-        sudo apt install $name -y
-        echo "Installation Completed..!"
-    else
-        echo "Exited the Script."
-    fi
-fi
+done
